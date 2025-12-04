0df34355f004b273aa7a7f5b6100235fe528ac3b
muflihanwar81-prog <muflihanwar81@gmail.com>
2025-12-05 03:38:28 +0700

    hapus semua file

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "buku_tamu");
-
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal: " . mysqli_connect_error();
-    exit();
-}
-?>
