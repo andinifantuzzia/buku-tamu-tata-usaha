54f51844b41b9085cb2589e533955371069dc402
muflihanwar81-prog <muflihanwar81@gmail.com>
2025-12-22 08:34:47 +0700

    Delete P10_3312501093 directory

--- P10_3312501093/hapus_dsn.php
+++
@@ -1,7 +0,0 @@
-<?php
-// include database connection file
-include 'koneksi.php';
-$nik = $_GET['nik'];
-$result = mysqli_query($koneksi, "DELETE FROM dosen WHERE nik='$nik'");
-header("Location: dosen.php");
-?>
--- P10_3312501093/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//hapus semua session
-session_unset();
-session_destory();
-// redirect ke halaman login
-header("location: login.php");
-exit();
-?>
